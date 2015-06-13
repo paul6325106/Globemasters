27bfcf25b70028c4a50840509f8d00ea08a7a7af
Paul Wade <paul6325106@gmail.com>
2015-06-13 14:26:49 +1000

    Added country entities without abbreviations

--- js/globe.js
+++ js/globe.js
@@ -67,7 +67,8 @@ function initialiseCesium(container, visualisation) {
     
     //load the country border geojson
     //XXX scene.pick doesn't seem to work if the entities are completely transparent, so use 0.01
-    viewer.dataSources.add(Cesium.GeoJsonDataSource.load('json/ne-countries-50m.json', {
+    viewer.dataSources.add(Cesium.GeoJsonDataSource.load(
+            "json/ne-countries-50m_no-abbreviations.json", {
         stroke: Cesium.Color.fromAlpha(Cesium.Color.WHITE, 0.75),
         fill: Cesium.Color.fromAlpha(Cesium.Color.WHITE, 0.01)
     }));
